d94ce035a09a9c1222d093f07734d2eb9946ceb7
Raxephion <Raxephion@gmail.com>
2022-07-07 14:16:13 +0200

    Add files via upload

---
+++ Alarm_clock.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  7 13:44:08 2022
+
+@author: Pierre-Henri Rossouw
+
+"60 Apps in 60 Days Challenge - App #2: Alarm Clock"
+
+"""
+
+from datetime import datetime
+from playsound import playsound
+
+
+alarm_time=input("Enter the time of the alarm to be set: HH:MM:SS\n")
+alarm_hour=alarm_time[0:2]
+alarm_minute=alarm_time[3:5]
+alarm_seconds=alarm_time[6:8]
+alarm_period=alarm_time[9:11].upper()
+print("Setting up alarm...")
+while True:
+    now=datetime.now()
+    current_hour=now.strftime("%I")
+    current_minute=now.strftime("%M")
+    current_seconds=now.strftime("%S")
+    current_period=now.strftime("%p")
+    if(alarm_period==current_period):
+        if(alarm_hour==current_hour):
+            if(alarm_minute==current_minute):
+                if(alarm_seconds==current_seconds):
+                    print("Wake Up!")
+                    playsound('audio.mp3')
+                    break
+                
